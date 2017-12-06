225c8bbb1016d7dc16816b90ce0a1d6d2c8615a6
Alexander Soen <u5642715@influence-map.novalocal>
2017-12-06 01:27:23 +0000

    Switch construct_db to api functions

--- python/construct_db.py
+++ python/construct_db.py
@@ -2,6 +2,7 @@ import sqlite3
 import os
 import csv
 from datetime import datetime 
+import construct_db_func
 
 # Input data directory
 data_dir = '/mnt/data/MicrosoftAcademicGraph'
@@ -14,26 +15,16 @@ ref_db_path = os.path.join(out_dir, 'paper_ref.db')
 conn = sqlite3.connect(ref_db_path)
 cur = conn.cursor()
 
+ref_name = "paper_ref"
+ref_colname = ["paper_id text", "paper_ref_id text"]
+ref_fileidx = [0, 1]
+ref_datapath = os.path.join(out_dir, 'paper_ref_test.db')
+
 # Create table
-print('{} creating table for paper_ref'.format(datetime.now()))
-cur.execute('DROP TABLE IF EXISTS paper_ref;')
-cur.execute('CREATE TABLE paper_ref (paper_id text, paper_ref_id text);')
-print('{} created table for paper_ref'.format(datetime.now()))
+construct_table(conn, ref_name, ref_colname)
 
 # Import table data from data
-print('{} importing data into paper_ref'.format(datetime.now()))
-with open(os.path.join(data_dir, 'data_txt/PaperReferences.txt')) as data:
-    line = data.readline()
-    cnt = 0
-    while line:
-        cnt += 1
-        vals = line.strip().split('\t')
-        cur.execute('INSERT INTO paper_ref (paper_id, paper_ref_id) VALUES (?, ?);', vals)
-        if cnt%1e7 == 0:
-            print('{} {:9.0f} lines of data imported into paper_ref'.format(datetime.now(), cnt))
-        line = data.readline()
-
-print('{} finished importing {:9.0f} lines of data into paper_ref'.format(datetime.now(), cnt))
+import_to_table(conn, ref_name, ref_datapath, ref_colname, ref_fileidx)
 
 # Index data
 print('{} indexing paper_id in paper_ref'.format(datetime.now()))
