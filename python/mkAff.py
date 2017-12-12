2082a6e5e19ba9ac31129b59ac4371f8c30312f5
Yuzhou Chen <u6363358@influence-map.novalocal>
2017-12-12 09:05:45 +0000

    Bug fixed (not tested)

--- python/mkAff.py
+++ python/mkAff.py
@@ -1,5 +1,4 @@
 import sqlite3
-import extractPubModule
 from datetime import datetime
 
 db_PAA = '/localdata/u5798145/influencemap/paper.db'
@@ -28,9 +27,37 @@ curA.close()
 
 author = {}
 
+def isSame(name1, name2):
+   ls2 = name2.split(' ')
+   ls1 = name1.split(' ')
+   middle1 = ls1[1:-1]
+   middle2 = ls2[1:-1]
+   if ls2[-1] == ls1[-1]:
+       if ls2[0] == ls1[0]:
+           return compareMiddle(middle1, middle2)
+       else:
+           return False
+   else:
+      return False
+
+
+def compareMiddle(middle1, middle2):
+    middleShort1 = ''
+    middleShort2 = ''
+    for char in middle1:
+       middleShort1 = middleShort1 + char[0]
+    for char in middle2:
+       middleShort2 = middleShort2 + char[0]
+    return (middleShort2 in middleShort1) or (middleShort1 in middleShort2)
+    
+
+
+
+
+
 print("{} matching the right name".format(datetime.now()))
 for a in allAuthor:
-    if extractPubModule.isSame(a[1],name):
+    if isSame(a[1],name):
           author[a[0]] = a[1]      
 
 result = []
@@ -39,22 +66,32 @@ aID = []
 for a in author:
    aID.append(a)
 
-curP.execute("SELECT authorID, paperID, affiliationNameOriginal FROM paperAuthorAffiliations WHERE authorID IN {}".format(tuple(aID)))
+curP.execute("SELECT authorID, paperID, affiliationNameOriginal FROM paperAuthorAffiliations WHERE authorID IN {} ".format(tuple(aID)))
 
 result = curP.fetchall()
 
+
 finalres = []
 
-for tup in result:
+for tup in result: 
     finalres.append((author[tup[0]], tup[0], tup[1], tup[2]))
+
+finalresult = []
+
+for tup in finalres:
+    currentA = tup[0]
+    count = 0
+    for tups in finalres:
+        if tups[0] == curentA:
+             count += 1
+    finalresult.append(tup[0],tup[1],count,tup[2])
+
 
 curP.close()
 print("{} done".format(datetime.now()))
 
-for tup in finalres:
+for tup in finalresult:
     print(tup)
-
-
 
 
 
