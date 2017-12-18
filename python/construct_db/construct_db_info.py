4b7e741f8ff25ee2e9f0ae984a275854271d6bce
Alexander Soen <u5642715@influence-map.novalocal>
2017-12-18 05:23:34 +0000

    Reorganise db construction files

--- python/construct_db/construct_db_info.py
+++ python/construct_db/construct_db_info.py
@@ -3,6 +3,8 @@ import os
 from datetime import datetime 
 import construct_db_config as cfg
 from construct_db_func import build_coltype, construct_table, create_index
+from construct_db_papers import construct_papers
+from construct_db_paa import construct_paa
 
 # Input data directory
 data_dir = cfg.data_dir
@@ -10,8 +12,6 @@ data_dir = cfg.data_dir
 # database path
 db_path = cfg.db_path
 
-conn = sqlite3.connect(db_path)
-cur = conn.cursor()
 
 # Create count table
 # Table details
@@ -20,30 +20,42 @@ table_col = ['paper_id', 'auth_count']
 table_type = ['text', 'int']
 table_coltype = build_coltype(table_col, table_type)
 
-# Construct table
-construct_table(conn, table_name, table_coltype, override=True)
+def construct_paper_info():
+    conn = sqlite3.connect(db_path)
+    cur = conn.cursor()
 
-# Count the number of authors per paper
-print('{} start count authors per paper'.format(datetime.now()))
-cur.execute('INSERT INTO authcount (paper_id, auth_count) SELECT paper_id, Count(*) FROM paa GROUP BY paper_id;')
-print('{} finish count authors per paper'.format(datetime.now()))
+    # Construct dependent tables
+    construct_papers()
+    construct_paa()
 
-# Index authcount for faster join
-create_index(conn, table_name, table_col[0])
+    # Construct table
+    construct_table(conn, table_name, table_coltype, override=True)
 
-# Join tables together with count
-print('{} start join paa with authcount'.format(datetime.now()))
-cur.execute('CREATE TABLE paa_count AS SELECT a.paper_id, auth_id, auth_count, affi_id FROM paa a INNER JOIN authcount b ON a.paper_id = b.paper_id;')
-print('{} finish join paa with authcount'.format(datetime.now()))
+    # Count the number of authors per paper
+    print('{} start count authors per paper'.format(datetime.now()))
+    cur.execute('INSERT INTO authcount (paper_id, auth_count) SELECT paper_id, Count(*) FROM paa GROUP BY paper_id;')
+    print('{} finish count authors per paper'.format(datetime.now()))
 
-# Join the two constructed tables to make required paper_info table
-print('{} start joining papers and paa by paper_id'.format(datetime.now()))
-cur.execute('CREATE TABLE paper_info AS SELECT a.paper_id, auth_id, auth_count, conf_id, affi_id FROM papers a INNER JOIN paa_count b ON a.paper_id = b.paper_id;')
-print('{} finish joining papers and paa by paper_id'.format(datetime.now()))
+    # Index authcount for faster join
+    create_index(conn, table_name, table_col[0])
 
-# Index paper_info wrt paper_id
-create_index(conn, 'paper_info', table_col[0])
+    # Join tables together with count
+    print('{} start join paa with authcount'.format(datetime.now()))
+    cur.execute('CREATE TABLE paa_count AS SELECT a.paper_id, auth_id, auth_count, affi_id FROM paa a INNER JOIN authcount b ON a.paper_id = b.paper_id;')
+    print('{} finish join paa with authcount'.format(datetime.now()))
 
-# Save
-conn.commit()
-conn.close()
+    # Join the two constructed tables to make required paper_info table
+    print('{} start joining papers and paa by paper_id'.format(datetime.now()))
+    cur.execute('CREATE TABLE paper_info AS SELECT a.paper_id, auth_id, auth_count, conf_id, affi_id FROM papers a INNER JOIN paa_count b ON a.paper_id = b.paper_id;')
+    print('{} finish joining papers and paa by paper_id'.format(datetime.now()))
+
+    # Index paper_info wrt paper_id
+    create_index(conn, 'paper_info', table_col[0])
+
+    # Save
+    conn.commit()
+    cur.close()
+    conn.close()
+
+if __name__ == '__main__':
+    construct_paper_info()
