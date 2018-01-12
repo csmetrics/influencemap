d039091c66501efb6e70c9075272d5ffa26def79
Benjamin Readshaw <u5798145@influence-map.novalocal>
2018-01-12 04:32:10 +0000

    fix front end for conference and journals (partially institutions)

---
+++ webapp/webapp/static/js/searchTableConfig.js
@@ -0,0 +1,142 @@
+var searchTableConfig1 = {
+  author: {
+    tableSettings: {
+      "pageLength": 10, // default page
+      "lengthMenu": [ 10, 20, 50, 100 ], // page options
+      "lengthChange": true,
+      "columns": [
+      { title: "Name", className: "index dt-body-center" },
+      { title: "Number of papers", className: "institution dt-body-right" },
+      { title: "Affiliations", className: "dt-body-center" },
+      { title: "Fields", className: "dt-body-left" },
+      { title: "Papers", className: "dt-body-left" },
+      { title: "ID"}
+      ],
+      "columnDefs": [
+      {
+        "targets": 5,
+        "visible": false
+      },
+      ],
+      "select": {
+        "style": "multi"
+      },
+      "language": {
+        "emptyTable": "no search result"
+      },
+      "fixedColumns": true,
+      "paging": true,
+      "pagingType": "simple_numbers",
+      "ordering": true,
+      "order": [[1, "desc" ]],
+      "info": true,
+      "autoWidth": false,
+    },
+    responseKeys: [
+    'name', 'numpaper', 'affiliation','field','recentPaper','authorID'
+    ], 
+    idCol: 5
+  },
+  conference: {
+    tableSettings: {
+      "pageLength": 10, // default page
+      "lengthMenu": [ 10, 20, 50, 100 ], // page options
+      "lengthChange": true,
+      "columns": [
+      { title: "Name", className: "index dt-body-center" },
+      { title: "ID"}
+      ],
+      "columnDefs": [
+      {
+        "targets": 1,
+        "visible": false
+      },
+      ],
+      "select": {
+        "style": "multi"
+      },
+      "language": {
+        "emptyTable": "no search result"
+      },
+      "fixedColumns": true,
+      "paging": true,
+      "pagingType": "simple_numbers",
+      "ordering": true,
+      "order": [[0, "asc" ]],
+      "info": true,
+      "autoWidth": false,
+    },
+    responseKeys: [
+    'name', 'id'
+    ],
+    idCol: 1
+  },
+  institution: {
+    tableSettings: {
+      "pageLength": 10, // default page
+      "lengthMenu": [ 10, 20, 50, 100 ], // page options
+      "lengthChange": true,
+      "columns": [
+      { title: "Name", className: "index dt-body-center" },
+      { title: "ID"}
+      ],
+      "columnDefs": [
+      {
+        "targets": 1,
+        "visible": false
+      },
+      ],
+      "select": {
+        "style": "multi"
+      },
+      "language": {
+        "emptyTable": "no search result"
+      },
+      "fixedColumns": true,
+      "paging": true,
+      "pagingType": "simple_numbers",
+      "ordering": true,
+      "order": [[0, "asc" ]],
+      "info": true,
+      "autoWidth": false,
+    },
+    responseKeys: [
+    'name', 'id'
+    ],
+    idCol: 1
+  },
+  journal: {
+    tableSettings: {
+      "pageLength": 10, // default page
+      "lengthMenu": [ 10, 20, 50, 100 ], // page options
+      "lengthChange": true,
+      "columns": [
+      { title: "Name", className: "index dt-body-center" },
+      { title: "ID"}
+      ],
+      "columnDefs": [
+      {
+        "targets": 1,
+        "visible": false
+      },
+      ],
+      "select": {
+        "style": "multi"
+      },
+      "language": {
+        "emptyTable": "no search result"
+      },
+      "fixedColumns": true,
+      "paging": true,
+      "pagingType": "simple_numbers",
+      "ordering": true,
+      "order": [[0, "asc" ]],
+      "info": true,
+      "autoWidth": false,
+    },
+    responseKeys: [
+    'name', 'id'
+    ],
+    idCol: 1
+  }
+};
