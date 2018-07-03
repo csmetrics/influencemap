c78c1dc71bbb2d5794942ac02dd94d544d5970be
Minjeong Shin <minjeong.shin@anu.edu.au>
2018-07-03 22:16:14 +1000

    add pub/cite barchart and slider in the middle

--- webapp/webapp/static/js/statchart.js
+++ webapp/webapp/static/js/statchart.js
@@ -8,22 +8,25 @@ class PubChart {
   }
 
   initChart(data, opt) {
-    var height = opt.height-opt.top-opt.bottom;
+    var height = opt.height-opt.top;
     var width = opt.width-opt.left-opt.right;
-
     this.g = this.chartsvg.append("g")
         .attr("transform", "translate(" + opt.left + "," + opt.top + ")");
     var x = d3.scaleBand().range([0, width]).padding(0.2);
     var pub_y = d3.scaleLinear().range([height, 0]);
 
     x.domain(data.map(function(d) { return d.year; }));
-    pub_y.domain([0, d3.max(data, function(d) { return d.value; })]);
+    pub_y.domain([0, d3.max(data, function(d) { return d.value+1; })]);
 
     // publication chart
     this.g.append("g")
       .attr("class", "axis axis--x")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x).tickValues(d3.range(opt.min, opt.max, opt.xticks)));
+      // .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisTop(x).tickValues(d3.range(opt.min, opt.max, opt.xticks)));
+    this.g.append("g") // x axis - grid
+      .attr("class", "grid")
+      .call(d3.axisBottom(x)
+        .tickSize(height).tickFormat("").ticks(opt.xticks));
 
     this.g.append("g")
       .attr("class", "axis axis--y")
@@ -34,6 +37,10 @@ class PubChart {
       .attr("dy", "0.71em")
       .attr("text-anchor", "end")
       .text("Number of Publications");
+    this.g.append("g") // y axis - grid
+      .attr("class", "grid")
+      .call(d3.axisLeft(pub_y)
+        .tickSize(-width).tickFormat("").ticks(opt.yticks));
 
     this.g.selectAll(".bar-pub")
       .data(data)
@@ -71,30 +78,30 @@ class CiteChart {
   constructor(div_id) {
     this.chartsvg = d3.select(div_id).append("svg")
                   .attr("width", "100%");
-    this.initflag = false;
   }
 
-  isInitialized(){
-    return this.initflag;
-  }
-
-  initChart(data, opt) {
-    var height = opt.height-opt.top-opt.bottom;
+  drawChart(data, opt) {
+    var height = opt.height-opt.bottom;
     var width = opt.width-opt.left-opt.right;
+    this.chartsvg.selectAll("g").remove();
     this.g = this.chartsvg.append("g")
-        .attr("transform", "translate(" + opt.left + "," + opt.top + ")");
+        .attr("transform", "translate(" + opt.left + "," + 0 + ")");
 
     var x = d3.scaleBand().range([0, width]).padding(0.2);
     var cit_y = d3.scaleLinear().range([height, 0]);
 
     x.domain(data.map(function(d) { return d.year; }));
-    cit_y.domain([0, d3.max(data, function(d) { return d.value; })]);
+    cit_y.domain([0, d3.max(data, function(d) { return d.value+5; })]);
 
     // citation chart
     this.g.append("g")
       .attr("class", "axis axis--x")
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x).tickValues(d3.range(opt.min, opt.max, opt.xticks)));
+    this.g.append("g") // x axis - grid
+      .attr("class", "grid")
+      .call(d3.axisBottom(x)
+        .tickSize(height).tickFormat("").ticks(opt.xticks));
 
     this.g.append("g")
       .attr("class", "axis axis--y")
@@ -105,6 +112,10 @@ class CiteChart {
       .attr("dy", "0.71em")
       .attr("text-anchor", "end")
       .text("Number of Citations");
+    this.g.append("g") // y axis - grid
+      .attr("class", "grid")
+      .call(d3.axisLeft(cit_y)
+        .tickSize(-width).tickFormat("").ticks(opt.yticks));
 
     this.g.selectAll(".bar-cite")
       .data(data)
@@ -114,26 +125,6 @@ class CiteChart {
       .attr("y", function(d) { return cit_y(d.value); })
       .attr("width", x.bandwidth())
       .attr("height", function(d) { return (height-cit_y(d.value)); })
-      .style("fill", "blue");
-
-    this.initflag = true;
-  }
-
-  updateRange(data, opt) {
-    var vheight = opt.height-opt.top-opt.bottom;
-    var vwidth = opt.width-opt.left-opt.right;
-    console.log(vheight, vwidth)
-    var vx = d3.scaleBand().range([0, vwidth]).padding(0.2);
-    var vcit_y = d3.scaleLinear().range([vheight, 0]);
-    // this.g.selectAll(".bar-cite").remove();
-    this.g.selectAll(".bar-cite")
-      .data(data)
-      .enter().append("rect")
-      .attr("class", "bar-cite")
-      .attr("x", function(d) { return vx(d.year); })
-      .attr("y", function(d) { return vcit_y(d.value); })
-      .attr("width", vx.bandwidth())
-      .attr("height", function(d) { return (vheight-vcit_y(d.value)); })
       .style("fill", "blue");
   }
 }
