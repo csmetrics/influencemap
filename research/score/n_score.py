82063a9ceb4472e770f07fd3a1805c9fca81ba09
Alexander Soen <u5642715@influence-map.novalocal>
2018-07-04 08:02:18 +0000

    Add research scripts, processing name to info

--- research/score/n_score.py
+++
@@ -1,9 +0,0 @@
-'''
-date:   30.06.18
-author: Alexander Soen
-'''
-
-def n_score_paper_info_list(paper_info_list):
-    ''' Generates the n score (number of papers) for paper information.
-    '''
-    return len(paper_info_list)
