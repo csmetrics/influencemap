12ab9f9f66f3c65fe864c99e848d0949f0ad64e6
Minjeong Shin <minjeong.shin@anu.edu.au>
2018-07-11 11:38:35 +1000

    change webhook, printing format

--- misc/monitor/report_number.sh
+++ misc/monitor/report_number.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ES_ADDR=http://130.56.248.105:9200
 DIR=/mnt/influencemap/misc/monitor
-WEBHOOK=https://hooks.slack.com/services/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
+WEBHOOK=https://www.cmlab.ml/hooks/fi1y5pxaztne5dj7demtpsihdo
 
 cd $DIR
 indices=("journals" "browse_author_group" "browse_paper_group" "conferenceinstances" \
@@ -11,7 +11,7 @@ indices=("journals" "browse_author_group" "browse_paper_group" "conferenceinstan
 # set refresh_interval to 1s
 for idx in "${indices[@]}"
 do
-  curl -XPUT $ES_ADDR/$idx/_settings -H 'Content-type: application/json' -d '{ "index": { "refresh_interval": "1s"  }}'
+    curl -XPUT $ES_ADDR/$idx/_settings -H 'Content-type: application/json' -d '{ "index": { "refresh_interval": "1s"  }}'
 done
 sleep 1m
 
@@ -22,10 +22,11 @@ do
     curl -XGET $ES_ADDR/$idx/_count > counts/$idx.now.json
 done
 python compare.py > output
+sed -i "s/'/\"/g" output
 curl -X POST -H 'Content-type: application/json' --data @output $WEBHOOK
 
 # revert refresh_interval to -1
 for idx in "${indices[@]}"
 do
-  curl -XPUT $ES_ADDR/$idx/_settings -H 'Content-type: application/json' -d '{ "index": { "refresh_interval": "-1"  }}'
+    curl -XPUT $ES_ADDR/$idx/_settings -H 'Content-type: application/json' -d '{ "index": { "refresh_interval": "-1"  }}'
 done
