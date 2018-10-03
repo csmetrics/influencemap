aecf1a6a58686b189778a110d7af1160d7ada1f3
Alexander Soen <alexandersoen@hotmail.com>
2018-10-03 18:59:12 +0000

    Remove API from creating cache
    - Need to solve missing papers case
    - Stress Test
    - Getting paper reference is slow, but may be consequence of index being
      updated
    - Possible to make threads for elasticsearch

--- core/search/query_info.py
+++ core/search/query_info.py
@@ -9,6 +9,7 @@ author: Alexander Soen
 from core.search.cache_data       import cache_paper_info
 from core.search.query_info_cache import paper_info_cache_query
 from core.search.query_info_db    import paper_info_db_query
+from core.search.query_info_db    import paper_info_multiquery
 from core.search.query_info_mag   import paper_info_mag_multiquery
 from core.search.query_utility    import chunker
 from itertools import zip_longest
@@ -76,6 +77,43 @@ def paper_info_mag_check_multiquery(paper_ids, force=False):
             cache_paper_info(process_res)
             cache_paper_info(partial_res, chunk_size=100)
             paper_info_res += process_res
+
+    return paper_info_res
+
+
+def paper_info_db_check_multiquery(paper_ids, force=False):
+    ''' Query which checks cache for existence first. Otherwise tries to
+        generate paper info from basic tables and adds to cache.
+    '''
+    # Find entries in ES
+    if force:
+        to_add_links   = list()
+        to_process     = paper_ids
+        paper_info_res = list()
+    else:
+        es_res = paper_info_cache_query(paper_ids)
+        to_add_links   = es_res['partial']
+        to_process     = list(es_res['missing'])
+        paper_info_res = es_res['complete']
+
+    print("Complete cache entries found:", len(paper_info_res))
+    print("Partial cache entries found:", len(to_add_links))
+    print("No cache entries found:", len(to_process))
+    print("Total ids to query:", len(paper_ids))
+
+    # Check if items do not exist in cache
+    if to_process or to_add_links:
+        total_res, partial_res = paper_info_multiquery(to_process,
+                                       partial_info=to_add_links)
+
+        complete_res = [t for t in total_res if t['cache_type'] == 'complete']
+        print("Complete cached:", len(total_res))
+        print("Partial cached:", len(partial_res))
+
+        # Cache
+        cache_paper_info(complete_res)
+        cache_paper_info(partial_res, chunk_size=100)
+        paper_info_res += total_res
 
     return paper_info_res
 
--- core/search/query_info_db.py
+++ core/search/query_info_db.py
@@ -10,46 +10,50 @@ date:   24.06.18
 author: Alexander Soen
 '''
 
+import copy
+from datetime import datetime
+
 from graph.config import conf
 from core.search.query_info_cache import base_paper_cache_query
 
 from elasticsearch import Elasticsearch
-from elasticsearch_dsl import Search
+from elasticsearch_dsl import Search, Q
 
-def papers_prop_query(paper_id):
+TIMEOUT = 60
+
+def papers_prop_query(paper_ids):
     ''' Get properties of a paper.
     '''
     # Elastic search client
     client = Elasticsearch(conf.get("elasticsearch.hostname"))
 
     # Targets
-    papers_targets = ['PaperId', 'ConferenceInstanceId', 'JournalId', 'Year']
-
-    # Query results
-    papers_res = dict()
+    papers_targets = ['ConferenceInstanceId', 'JournalId', 'Year']
 
     # Query for papers
     papers_s = Search(index = 'papers', using = client)
-    papers_s = papers_s.query('match', PaperId = paper_id)
+    papers_s = papers_s.query('terms', _id=paper_ids)
     papers_s = papers_s.source(papers_targets)
+    papers_s = papers_s.params(request_timeout=TIMEOUT)
 
     # Convert papers into dictionary format
+    results = dict()
     for paper in papers_s.scan():
-        # Parse results to dictionaries
-        for target in papers_targets:
-            try:
-                papers_res[target] = paper[target]
-            except KeyError:
-                pass
+        # Get properties for papers
+        paper_res = paper.to_dict()
+        p_id = int(paper.meta.id)
 
-        # There only should be a single results per paper id
-        break
+        # Rename Conference
+        if 'ConferenceInstanceId' in paper_res:
+            paper_res['ConferenceId'] = paper_res.pop('ConferenceInstanceId')
 
-    # Check for no results and return
-    return papers_res if papers_res else None
+        paper_res['PaperId'] = p_id
+        results[p_id] = paper_res
+
+    return results
 
 
-def paa_prop_query(paper_id):
+def paa_prop_query(paper_ids):
     ''' Get properties of a paper.
     '''
     
@@ -57,35 +61,34 @@ def paa_prop_query(paper_id):
     client = Elasticsearch(conf.get("elasticsearch.hostname"))
 
     # Targets
-    paa_targets = ['AuthorId', 'AffiliationId']
-
-    # Query results
-    paa_list = list()
+    paa_targets = ['PaperId', 'AuthorId', 'AffiliationId']
 
     # Query for paper affiliation
     paa_s = Search(index = 'paperauthoraffiliations', using = client)
-    paa_s = paa_s.query('match', PaperId = paper_id)
+    paa_s = paa_s.query('terms', PaperId=paper_ids)
     paa_s = paa_s.source(paa_targets)
+    paa_s = paa_s.params(request_timeout=TIMEOUT)
 
     # Convert paa into dictionary format
+    results = dict()
     for paa in paa_s.scan():
-        row_dict = dict()
+        paa_res = paa.to_dict()
 
-        # Parse results to dictionaries
-        for target in paa_targets:
-            try:
-                row_dict[target] = paa[target]
-            except KeyError:
-                pass
+        # Get fields
+        paper_id = paa_res['PaperId']
+        del paa_res['PaperId']
 
-        # Add to query list
-        paa_list.append(row_dict)
+        # Aggregate results
+        if paper_id in results:
+            results[paper_id].append(paa_res)
+        else:
+            results[paper_id] = [paa_res]
 
     # Return as dictionary
-    return {'Authors': paa_list}
+    return results
     
 
-def pr_links_query(paper_id):
+def pr_links_query(paper_ids):
     ''' Get properties of a paper.
     '''
     # Elastic search client
@@ -98,39 +101,59 @@ def pr_links_query(paper_id):
     references = list()
     citations  = list()
 
+    # Result dictionary
+    results = dict()
+    for paper_id in paper_ids:
+        results[paper_id] = {'References': [], 'Citations': []}
+
     # Query for paper references
-    pref_s = Search(index = 'paperreferences', using = client)
-    pref_s = pref_s.query('multi_match', query = paper_id, fields = pr_targets)
+    ref_s = Search(index = 'paperreferences', using = client)
+    ref_s = ref_s.query('terms', PaperId=paper_ids)
+    ref_s = ref_s.params(request_timeout=TIMEOUT)
 
     # Convert into dictionary format
-    for cite_info in pref_s.scan():
+    for ref_info in ref_s.scan():
+        results[ref_info[pr_targets[0]]]['References'].append(ref_info[pr_targets[1]])
 
-        # Determine what type of citation
-        if paper_id == cite_info[pr_targets[0]]:
-            references.append(cite_info[pr_targets[1]])
-        else:
-            citations.append(cite_info[pr_targets[0]])
+    # Query for paper citations
+    cit_s = Search(index = 'paperreferences', using = client)
+    cit_s = cit_s.query('terms', PaperReferenceId=paper_ids)
+    cit_s = cit_s.params(request_timeout=TIMEOUT)
+
+    # Convert into dictionary format
+    for cit_info in cit_s.scan():
+        results[cit_info[pr_targets[1]]]['Citations'].append(cit_info[pr_targets[0]])
 
     # Return results as a dictionary
-    return {'References': references, 'Citations': citations}
+    return results
 
 
-def base_paper_db_query(paper_id):
+def base_paper_db_query(paper_ids):
     ''' Generates basic paper_info dictionary/json from basic databases (not
         cache).
     '''
-    # Get paper ids
-    papers_prop = papers_prop_query(paper_id)
+    t_cur = datetime.now()
+    # Get basic paper properties
+    papers_props = papers_prop_query(paper_ids)
+    print('Get basic props', datetime.now() - t_cur)
 
-    # Check for empty results
-    if not papers_prop:
-        return None
 
+    t_cur = datetime.now()
     # Get author information
-    paa_prop = paa_prop_query(paper_id)
+    paa_props = paa_prop_query(paper_ids)
+    print('Get paa', datetime.now() - t_cur)
 
-    # Combine results for the basic paper information
-    return dict(papers_prop, **paa_prop)
+    # Create partial entry
+    results = list()
+    for paper_id in paper_ids:
+        if paper_id in papers_props and paper_id in paa_props:
+            # Make partial result
+            partial_res = papers_props[paper_id]
+            partial_res['Authors'] = paa_props[paper_id]
+
+            results.append(partial_res)
+
+    return results
 
 
 def link_paper_info_query(paper_id, cache = True):
@@ -187,6 +210,86 @@ def paper_info_db_query(paper_id):
     
     # Return paper_info
     return paper_info
+
+
+def paper_info_multiquery(paper_ids, partial_info=list()):
+    '''
+    '''
+    # Create partial information dictionary
+    paper_partial = dict()
+    for p_info in partial_info:
+        paper_partial[p_info['PaperId']] = p_info
+
+    # List of papers we are interested in returning total results
+    search_papers = paper_ids + list(paper_partial.keys())
+    
+    t_cur = datetime.now()
+    # Find all paper links
+    paper_links = pr_links_query(search_papers)
+    print('Get links', datetime.now() - t_cur)
+
+    # Calculate papers to query es for partials
+    find_partial = set(paper_ids)
+    for paper_link in paper_links.values():
+        find_partial.update(paper_link['References'])
+        find_partial.update(paper_link['Citations'])
+
+    print("Need to find,", len(find_partial))
+
+    # Get partial information for papers from cache
+    for p_info in base_paper_cache_query(list(find_partial)):
+        p_id = p_info['PaperId']
+        find_partial.remove(p_id)
+        paper_partial[p_id] = p_info
+
+    # Get partial information from db
+    for p_info in base_paper_db_query(list(find_partial)):
+        #print(p_info)
+        p_id = p_info['PaperId']
+        find_partial.remove(p_id)
+        paper_partial[p_id] = p_info
+
+    # Handle infos which cannot be found here TODO
+    print(find_partial)
+    print('Missing from db', len(find_partial))
+    for p_id in find_partial:
+        paper_partial[p_id] = {'PaperId': p_id, 'Year': 2018, 'Authors': []}
+    
+    # Generate results
+    total_res   = list()
+    partial_res = list()
+    for p_id, p_partial in paper_partial.items():
+
+        # TEMP FOR INCOMPLETE
+        if p_id in find_partial:
+            continue
+
+        p_partial = copy.deepcopy(p_partial)
+
+        # If links exists and is a search paper
+        if p_id in search_papers and p_id in paper_links:
+            p_partial['cache_type'] = 'complete'
+
+            # Create references and citations for total paper
+            p_links = {'References': [], 'Citations': []}
+
+            for r_id in paper_links[p_id]['References']:
+                p_links['References'].append(paper_partial[r_id])
+                if r_id in find_partial:
+                    p_partial['cache_type'] = 'taint'
+
+            for c_id in paper_links[p_id]['Citations']:
+                p_links['Citations'].append(paper_partial[c_id])
+                if c_id in find_partial:
+                    p_partial['cache_type'] = 'taint'
+
+            total_res.append(dict(p_partial, **p_links))
+        # Otherwise just add as partial cache entry
+        else:
+            p_partial['cache_type'] = 'partial'
+            partial_res.append(p_partial)
+
+    return total_res, partial_res 
 
 
 if __name__ == '__main__':
