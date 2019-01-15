53894ff10ca049d8239edce20739b6c1a6394063
Minjeong Shin <minjeong.shin@anu.edu.au>
2019-01-15 17:22:32 +1100

    ignore if index does not exist

--- misc/monitor/compare.py
+++ misc/monitor/compare.py
@@ -8,11 +8,14 @@ output = "|{}|{}|{}|\n".format("index", "changes", "total")
 output += "|---:|---:|---:|\n"
 # print("{:>25}\t{:>12}\t{:>12}".format("index", "changes", "total"))
 for data in data_now:
-    now = json.loads(open(os.path.join(data_dir, data)).read())
-    prev = json.loads(open(os.path.join(data_dir, data.replace("now", "prev"))).read())
-    if "count" in now and "count" in prev:
-        output += "|*{}*| {:,}| {:,}|\n".format(data.split('.')[0], now["count"]-prev["count"], now["count"])
-    # print("{:>25}\t{:>12,}\t{:>12,}".format(data.split('.')[0], now["count"]-prev["count"], now["count"]))
+    try:
+        now = json.loads(open(os.path.join(data_dir, data)).read())
+        prev = json.loads(open(os.path.join(data_dir, data.replace("now", "prev"))).read())
+        if "count" in now and "count" in prev:
+            output += "|*{}*| {:,}| {:,}|\n".format(data.split('.')[0], now["count"]-prev["count"], now["count"])
+        # print("{:>25}\t{:>12,}\t{:>12,}".format(data.split('.')[0], now["count"]-prev["count"], now["count"]))
+    except:
+        pass
 print({
     "text": output
 })
