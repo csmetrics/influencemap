7035270cf964b426179f390accbd31ed1fd5534b
Jakub Nabaglo <j@nab.gl>
2021-03-22 14:33:14 +1100

    Optimize agg_score

--- core/score/agg_score.py
+++ core/score/agg_score.py
@@ -23,37 +23,30 @@ SCORE_COLS = [
     'entity_name', 'influenced', 'influencing', 'self_cite', 'coauthor',
     ]
 
-AGG_COLS = [
-    'entity_name',
-    ]
+AGG_COL = 'entity_name'
 
 AGG_FUNC = {
-    'coauthor': np.any,
-    'influenced_tot': np.nansum,
-    'influencing_tot': np.nansum,
-    'influenced_nsc': np.nansum,
-    'influencing_nsc': np.nansum,
+    'coauthor': 'any',
+    'influenced_tot': 'sum',
+    'influencing_tot': 'sum',
+    'influenced_nsc': 'sum',
+    'influencing_nsc': 'sum',
     }
 
 SCORE_PREC = 5
 
-def safe_ratio(ratio_func, inf_type, row):
+def safe_ratio(df, inf_type):
+    res = (df['influencing_{}'.format(inf_type)]
+            - df['influenced_{}'.format(inf_type)])
+    res /= df['sum_{}'.format(inf_type)]
+    res[df['sum_{}'.format(inf_type)] == 0] = 0
+    return res
 
-    if row['sum_{}'.format(inf_type)] == 0:
-        return 0
-
-    ratio = ratio_func(
-        row['influencing_{}'.format(inf_type)],
-        row['influenced_{}'.format(inf_type)])
-
-    ratio /= row['sum_{}'.format(inf_type)]
-    return ratio
-
-def agg_score_df(
-        influence_df, coauthors=set([]), ratio_func=np.subtract,
-        sort_func=np.maximum):
+def agg_score_df(influence_df):
     """ Aggregates the scoring generated from ES paper information values.
     """
+
+
 
     print('\n---\n{} start score generation'.format(datetime.now()))
     if influence_df.empty:
@@ -69,19 +62,21 @@ def agg_score_df(
     score_df.loc[~score_df.self_cite, 'influenced_nsc'] = score_df.influenced_tot
     score_df.loc[~score_df.self_cite, 'influencing_nsc'] = score_df.influencing_tot
 
+    # Fill nan values to zero
+    score_df['influenced_tot']  = score_df['influenced_tot'].fillna(0)
+    score_df['influencing_tot'] = score_df['influencing_tot'].fillna(0)
+    score_df['influenced_nsc']  = score_df['influenced_nsc'].fillna(0)
+    score_df['influencing_nsc'] = score_df['influencing_nsc'].fillna(0)
+
+    # breakpoint()
+
     # Aggregate scores up
-    score_df = score_df.groupby(AGG_COLS).agg(AGG_FUNC).reset_index()
+    score_df = score_df.groupby(AGG_COL).agg(AGG_FUNC).reset_index()
 
     score_df.loc[~score_df.coauthor, 'influenced_nca'] = score_df.influenced_tot
     score_df.loc[~score_df.coauthor, 'influencing_nca'] = score_df.influencing_tot
     score_df.loc[~score_df.coauthor, 'influenced_nscnca'] = score_df.influenced_nsc
     score_df.loc[~score_df.coauthor, 'influencing_nscnca'] = score_df.influencing_nsc
-
-    # Fill nan values to zero
-    score_df['influenced_nca']  = score_df['influenced_nca'].fillna(0)
-    score_df['influencing_nca'] = score_df['influencing_nca'].fillna(0)
-    score_df['influenced_nscnca']  = score_df['influenced_nscnca'].fillna(0)
-    score_df['influencing_nscnca'] = score_df['influencing_nscnca'].fillna(0)
 
     # Limit precision of scoring
     score_df['influenced_tot']  = score_df.influenced_tot.round(SCORE_PREC)
@@ -100,17 +95,10 @@ def agg_score_df(
     score_df['sum_nscnca'] = score_df.influenced_nscnca + score_df.influencing_nscnca
 
     # calculate influence ratios
-    score_df['ratio_tot'] = score_df.apply(
-        lambda r: safe_ratio(ratio_func, 'tot', r), axis=1)
-
-    score_df['ratio_nsc'] = score_df.apply(
-        lambda r: safe_ratio(ratio_func, 'nsc', r), axis=1)
-
-    score_df['ratio_nca'] = score_df.apply(
-        lambda r: safe_ratio(ratio_func, 'nca', r), axis=1)
-
-    score_df['ratio_nscnca'] = score_df.apply(
-        lambda r: safe_ratio(ratio_func, 'nscnca', r), axis=1)
+    score_df['ratio_tot'] = safe_ratio(score_df, 'tot')
+    score_df['ratio_nsc'] = safe_ratio(score_df, 'nsc')
+    score_df['ratio_nca'] = safe_ratio(score_df, 'nca')
+    score_df['ratio_nscnca'] = safe_ratio(score_df, 'nscnca')
 
     print('{} finish score generation\n---'.format(datetime.now()))
 
