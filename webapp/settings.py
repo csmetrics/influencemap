7ef926e0b216a9e46e717c9efee4d041afb0fc1b
Jakub Nabaglo <j@nab.gl>
2021-05-04 18:01:11 +1000

    Connect Django with Postgres

--- webapp/settings.py
+++ webapp/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
