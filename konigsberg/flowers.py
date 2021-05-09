c263832b7797e5a460578c1d654c65f7bfdeab21
Jakub Nabaglo <j@nab.gl>
2021-05-09 18:33:11 +1000

    Cleanup + speedup

--- konigsberg/flowers.py
+++ konigsberg/flowers.py
@@ -1,92 +1,9 @@
 import json
+import mmap
 import pathlib
 
 import numba as nb
 import numpy as np
-
-from directed_graph import Graph
-
-ro_u4_arr = nb.types.Array(nb.u4, 1, 'C', readonly=True)
-
-
-@nb.jit(nb.f4[::1](nb.u4[::1], nb.f4[::1], ro_u4_arr),
-        nopython=True, nogil=True)
-def _weight_papers(paper_ids, paper_mul, paper_to_author_indptr):
-    retval = np.empty_like(paper_mul)
-    for i, (id_, mul) in enumerate(zip(paper_ids, paper_mul)):
-        num_authors = (paper_to_author_indptr[id_ + 1]
-                       - paper_to_author_indptr[id_])
-        if num_authors == 0:
-            num_authors = 1
-        retval[i] = mul / nb.f4(num_authors)
-    return retval
-
-
-@nb.jit(nb.types.Tuple([nb.u4[::1], nb.f4[::1]])(
-            nb.u4[::1], nb.u4[::1], nb.f4[::1], ro_u4_arr, ro_u4_arr),
-        nopython=True, nogil=True)
-def _filter_self_citations(
-    author_ids,
-    paper_ids, paper_muls,
-    paper_to_author_indptr, paper_to_author_indices
-):
-    new_paper_ids = []
-    new_paper_muls = []
-    for paper_id, paper_mul in zip(paper_ids, paper_muls):
-        start = paper_to_author_indptr[paper_id]
-        end = paper_to_author_indptr[paper_id + 1]
-        # Below equivalent to:
-        #     if not any(any(author_id == other_author_id
-        #                    for other_author_id
-        #                        in paper_to_author_indices[start:end])
-        #                for author_id in author_ids):
-        for author_id in author_ids:
-            if author_id in paper_to_author_indices[start:end]:
-                break
-        else:
-            new_paper_ids.append(paper_id)
-            new_paper_muls.append(paper_mul)
-    return np.array(new_paper_ids), np.array(new_paper_muls)
-
-
-@nb.jit(nb.types.Tuple([nb.u4[::1], nb.f4[::1]])(
-            nb.u4[::1], nb.u4[::1], nb.f4[::1]),
-        nopython=True, nogil=True)
-def _filter_ego(
-    author_ids_ego,
-    author_ids_other, author_muls_other,
-):
-    new_author_ids_other = []
-    new_author_muls_other = []
-    for author_id, author_mul in zip(author_ids_other, author_muls_other):
-        if author_id not in author_ids_ego:
-            new_author_ids_other.append(author_id)
-            new_author_muls_other.append(author_mul)
-    return np.array(new_author_ids_other), np.array(new_author_muls_other)
-
-
-@nb.jit(nb.types.Tuple([nb.u4[::1], nb.f4[::1]])(
-            nb.u4[::1], nb.u4[::1], nb.f4[::1], ro_u4_arr, ro_u4_arr),
-        nopython=True, nogil=True)
-def _filter_coauthors(
-    ego_paper_ids,
-    author_ids_other, author_muls_other,
-    p2a_ptr, p2a_idx,
-):
-    coauthors = set()
-    for paper_id in ego_paper_ids:
-        start = p2a_ptr[paper_id]
-        end = p2a_ptr[paper_id + 1]
-        coauthors.update(p2a_idx[start:end])
-
-    new_author_ids_other = []
-    new_author_muls_other = []
-    for author_id, author_mul in zip(author_ids_other, author_muls_other):
-        if author_id not in coauthors:
-            new_author_ids_other.append(author_id)
-            new_author_muls_other.append(author_mul)
-
-    return np.array(new_author_ids_other), np.array(new_author_muls_other)
 
 
 class Flower:
@@ -96,90 +13,160 @@ class Flower:
         self.influencees = influencees
 
 
+mmapped_arr = nb.types.Array(nb.u4, 1, 'C', readonly=True)
+mapping_arrs = nb.types.Tuple([mmapped_arr, mmapped_arr])
+
+
+@nb.jit(mmapped_arr(mapping_arrs, nb.u4), nopython=True, nogil=True)
+def traverse_one(mapping, i):
+    indptr, indices = mapping
+    start = indptr[i]
+    end = indptr[i + 1]
+    return indices[start:end]
+
+
+class Mapping:
+    __slots__ = ['indptr', 'indices', 'indptr_mmap', 'indices_mmap']
+
+    def __init__(self, path):
+        path = pathlib.Path(path)
+        with open(path / 'indptr.bin', 'rb') as f:
+            self.indptr_mmap = mmap.mmap(f.fileno(), 0, prot=mmap.PROT_READ)
+        self.indptr = np.frombuffer(self.indptr_mmap, dtype=np.uint32)
+        with open(path / 'indices.bin', 'rb') as f:
+            self.indices_mmap = mmap.mmap(f.fileno(), 0, prot=mmap.PROT_READ)
+        self.indices = np.frombuffer(self.indices_mmap, dtype=np.uint32)
+
+    def __del__(self):
+        self.indptr_mmap.close()
+        self.indices_mmap.close()
+
+    @property
+    def arrs(self):
+        return self.indptr, self.indices
+
+
 class Florist:
     def __init__(self, path):
         path = pathlib.Path(path)
-        self.author_to_paper = Graph(
-            path / 'author2paper-indptr.bin',
-            path / 'author2paper-indices.bin',
-        )
-        self.citee_to_citor = Graph(
-            path / 'citee2citor-indptr.bin',
-            path / 'citee2citor-indices.bin',
-        )
-        self.citor_to_citee = Graph(
-            path / 'citor2citee-indptr.bin',
-            path / 'citor2citee-indices.bin',
-        )
-        self.paper_to_author = Graph(
-            path / 'paper2author-indptr.bin',
-            path / 'paper2author-indices.bin',
-        )
+        self.author2paper = Mapping(path / 'author2paper')
+        self.citee2citor = Mapping(path / 'citee2citor')
+        self.citor2citee = Mapping(path / 'citor2citee')
+        self.paper2author = Mapping(path / 'paper2author')
         with open(path / 'paper-years.json') as f:
-            data = json.load(f)
-        self.year_starts = {int(year): start for year, start in data.items()}
-
-    def weight_papers(self, papers_with_mul):
-        paper_ids, paper_mul = papers_with_mul
-        new_mul = _weight_papers(paper_ids, paper_mul,
-                                 self.paper_to_author.indptr)
-        return paper_ids, new_mul
-
-    def filter_self_citations(self, author_ids, papers_with_mul):
-        paper_ids, paper_muls = papers_with_mul
-        return _filter_self_citations(
-            author_ids,
-            paper_ids, paper_muls,
-            self.paper_to_author.indptr, self.paper_to_author.indices)
+            self.year_starts = {
+                int(year): start
+                for year, start in json.load(f).items()}
         
-    def get_id_year_range(self, start, end):
-        return self.year_starts.get(start), self.year_starts.get(end)
+    def get_id_year_range(self, years):
+        if years is None:
+            return None
+
+        start_year, end_year = years
+        start_id = self.year_starts[start_year]
+        end_id = self.year_starts[end_year]
+        return nb.u4(start_id), nb.u4(end_id)
 
     def get_flower(
         self,
         author_ids,
         *,
-        self_citations=False,
-        coauthors=True,
-        pub_year_start=None, pub_year_end=None,
-        cit_year_start=None, cit_year_end=None,
+        self_citations=False, coauthors=True,
+        pub_years=None, cit_years=None,
     ):
-        pub_year_start, pub_year_end = self.get_id_year_range(
-            pub_year_start, pub_year_end)
-        cit_year_start, cit_year_end = self.get_id_year_range(
-            cit_year_start, cit_year_end)
+        pub_ids = self.get_id_year_range(pub_years)
+        cit_ids = self.get_id_year_range(cit_years)
 
-        origin = np.array(author_ids, dtype=np.uint32)
-        origin_papers = self.author_to_paper.traverse(
-            origin, id_start=pub_year_start, id_end=pub_year_end)
-        origin_papers = origin_papers[0], np.ones_like(origin_papers[1])
-        weighted_origin_papers = self.weight_papers(origin_papers)
-
-        citing_papers = self.citee_to_citor.traverse(
-            *weighted_origin_papers,
-            id_start=cit_year_start, id_end=cit_year_end)
-        if not self_citations:
-            citing_papers = self.filter_self_citations(origin, citing_papers)
-        cited_papers = self.citor_to_citee.traverse(
-            *origin_papers, id_start=pub_year_start, id_end=pub_year_end)
-        if not self_citations:
-            cited_papers = self.filter_self_citations(origin, cited_papers)
-        weighted_cited_papers = self.weight_papers(cited_papers)
-
-        influencers = self.paper_to_author.traverse(*weighted_cited_papers)
-        if coauthors:
-            influencers = _filter_ego(origin, *influencers)
-        else:
-            influencers = _filter_coauthors(
-                origin_papers[0],
-                influencers[0], influencers[1],
-                self.paper_to_author.indptr, self.paper_to_author.indices)
-        influencees = self.paper_to_author.traverse(*citing_papers)
-        if coauthors:
-            influencees = _filter_ego(origin, *influencees)
-        else:
-            influencees = _filter_coauthors(
-                origin_papers[0],
-                influencees[0], influencees[1],
-                self.paper_to_author.indptr, self.paper_to_author.indices)
+        influencers, influencees = _make_flower(
+            np.array(author_ids, dtype=np.uint32),
+            self.author2paper.arrs, self.paper2author.arrs,
+            self.citor2citee.arrs, self.citee2citor.arrs,
+            pub_ids, cit_ids,
+            nb.types.literal(bool(self_citations)),
+            nb.types.literal(bool(coauthors)),
+        )
         return Flower(influencers=influencers, influencees=influencees)
+
+
+@nb.jit(nopython=True, nogil=True)
+def _is_self_citation(ego_set, author_ids):
+    for author_id in author_ids:
+        if author_id in ego_set:
+            return True
+    return False
+
+
+@nb.jit(nopython=True, nogil=True)
+def _result_dict_to_arr(res):
+    ids = np.empty(len(res), dtype=nb.u4)
+    scores = np.empty(len(res), dtype=nb.f4)
+    for i, (id_, score) in enumerate(res.items()):
+        ids[i] = id_
+        scores[i] = score
+    return ids, scores
+
+
+@nb.generated_jit(nopython=True, nogil=True)
+def _is_in_range(range_, i):
+    if isinstance(range_, nb.types.NoneType):
+        return lambda range_, i: True
+    else:
+        return lambda range_, i: range_[0] <= i < range_[1]
+
+
+@nb.jit(nopython=True, nogil=True)
+def _make_flower(
+    author_ids,
+    author2paper, paper2author, citor2citee, citee2citor,
+    pub_ids, cit_ids,
+    self_citations, coauthors,
+):
+    ego_papers = set()
+    ego_set = set()
+    for author_id in author_ids:
+        for paper_id in traverse_one(author2paper, author_id):
+            if _is_in_range(pub_ids, paper_id):
+                ego_papers.add(paper_id)
+        ego_set.add(author_id)
+
+    excluded_authors = ego_set if coauthors else ego_set.copy()
+    citor_papers = nb.typed.Dict.empty(key_type=nb.u4, value_type=nb.f4)
+    citee_papers = nb.typed.Dict.empty(key_type=nb.u4, value_type=nb.u4)
+    for paper_id in ego_papers:
+        paper_authors = traverse_one(paper2author, paper_id)
+        num_authors = len(paper_authors)
+        if not coauthors and num_authors > 1:
+            excluded_authors.update(paper_authors)
+        recip_weight = nb.f4(1) / nb.f4(num_authors)
+        for citor_id in traverse_one(citee2citor, paper_id):
+            if _is_in_range(cit_ids, citor_id):
+                citor_papers[citor_id] = (
+                    citor_papers.get(citor_id, nb.f4(0.)) + recip_weight)
+        for citee_id in traverse_one(citor2citee, paper_id):
+            if _is_in_range(pub_ids, citee_id):
+                citee_papers[citee_id] = nb.u4(
+                    citee_papers.get(citee_id, nb.u4(0)) + nb.u4(1))
+
+    citor_authors = nb.typed.Dict.empty(key_type=nb.u4, value_type=nb.f4)
+    for paper_id, weight in citor_papers.items():
+        author_ids = traverse_one(paper2author, paper_id)
+        if not self_citations and _is_self_citation(ego_set, author_ids):
+            continue
+        for author_id in author_ids:
+            if author_id not in excluded_authors:
+                citor_authors[author_id] = (
+                    citor_authors.get(author_id, nb.f4(0.)) + weight)
+    
+    citee_authors = nb.typed.Dict.empty(key_type=nb.u4, value_type=nb.f4)
+    for paper_id, count in citee_papers.items():
+        author_ids = traverse_one(paper2author, paper_id)
+        if not self_citations and _is_self_citation(ego_set, author_ids):
+            continue
+        weight = nb.f4(count) / nb.f4(len(author_ids))
+        for author_id in author_ids:
+            if author_id not in excluded_authors:
+                citee_authors[author_id] = (
+                    citee_authors.get(author_id, nb.f4(0.)) + weight)
+
+    return (_result_dict_to_arr(citee_authors),
+            _result_dict_to_arr(citor_authors))
