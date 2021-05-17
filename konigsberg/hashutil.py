db6fc76afc3e2c7f3a630e150656f86ca16bc745
Jakub Nabaglo <j@nab.gl>
2021-05-17 11:05:18 +1000

    Document builder.

--- konigsberg/hashutil.py
+++ konigsberg/hashutil.py
@@ -1,3 +1,9 @@
+"""Utilities for mapping IDs to their indices.
+
+These are implemented as a hash map with open addressing/linear probing.
+See https://en.wikipedia.org/wiki/Linear_probing.
+"""
+
 import mmap
 
 import numba as nb
@@ -8,27 +14,40 @@ SENTINEL = np.uint32(-1)
 
 @nb.njit(nogil=True)
 def _convert_in2ind_inplace(id2ind, ind2id, id2ind_len, arr, allow_missing):
+    """Convert IDs to indices in-place.
+
+    id2ind is a Numpy array: a hash map mapping IDs to candidate
+    indices; id2ind_len is its length. ind2id maps indices to their IDs.
+    arr is the input/output array. When allow_missing is True,
+    unrecognised IDs do not raise, but are instead replaced with
+    SENTINEL.
+    """
     for i, id_ in enumerate(arr):
-        if id_ != SENTINEL:
-            hash_ = nb.u4(id_ * nb.u4(0x9e3779b1))
-            hash_ %= id2ind_len
-            while True:
-                index = id2ind[hash_]
-                if index == SENTINEL:
-                    if allow_missing:
-                        arr[i] = SENTINEL
-                        break
-                    else:
-                        raise KeyError('id not found')
-                if ind2id[index] == id_:
-                    arr[i] = index
-                    break
-                hash_ += 1
-                if hash_ >= id2ind_len:
-                    hash_ -= id2ind_len
+        hash_ = nb.u4(id_ * nb.u4(0x9e3779b1)) % id2ind_len
+        while True:
+            index = id2ind[hash_]
+            if index == SENTINEL:
+                if not allow_missing:
+                    raise KeyError('id not found')
+                arr[i] = SENTINEL
+                break
+            # id2ind[hash_] might be the correct index, but this is not
+            # guaranteed due to collisions. Look it up in ind2id to make
+            # sure.
+            if ind2id[index] == id_:
+                arr[i] = index
+                break
+            hash_ += 1  # id2ind[hash_] was not correct. Try next cell.
+            if hash_ >= id2ind_len:
+                hash_ = 0  # Went past the end; wrap around.
 
 
 class Ind2IdMap:
+    """Map from indices to IDs.
+
+    This is a plain NumPy array, where we retrieve the ID as
+    self.ind2id[index]. The entity type is not stored.
+    """
     def __init__(self, path):
         with open(path, 'rb') as f:
             self.ind2id_mmap = mmap.mmap(f.fileno(), 0, prot=mmap.PROT_READ)
@@ -39,6 +58,11 @@ class Ind2IdMap:
     
 
 class Id2IndHashMap:
+    """Map from IDs to indices.
+
+    A hash map maps IDs to _candidate_ indices. ind2id_map is needed to
+    check whether a candidate index is the correct result.
+    """
     def __init__(self, path, ind2id_map):
         with open(path, 'rb') as f:
             self.id2ind_mmap = mmap.mmap(f.fileno(), 0, prot=mmap.PROT_READ)
@@ -49,7 +73,14 @@ class Id2IndHashMap:
         self.id2ind_mmap.close()
 
     def convert_inplace(self, series, allow_missing=False):
+        """Convert IDs to indices in-place.
+
+        series is a Pandas series of indices. If allow_missing is set to
+        True, then unrecognised IDs do not raise KeyError, but are
+        instead replaced with SENTINEL.
+        """
         arr = series.to_numpy()
+        # nb.literally makes Numba compile once for every value.
         _convert_in2ind_inplace(
             self.id2ind, self.ind2id_map.ind2id,
             nb.literally(len(self.id2ind)),
@@ -63,7 +94,7 @@ class Id2IndHashMap:
             self.id2ind_mmap.close()
         self.id2ind_mmap = None
         self.id2ind = None
-        self.ind2id_map = None
+        self.ind2id_map = None  # Decrease ind2id_map refcount.
 
     def __enter__(self):
         pass
@@ -74,32 +105,50 @@ class Id2IndHashMap:
 
 @nb.njit(nogil=True)
 def _make_hash_map(ids, id2ind, arr_size, offset):
+    """Place IDs in the hash map.
+
+    ids is a NumPy array of IDs, ordered by the index. id2ind is the
+    target array; arr_size is its size. offset is a constant added to
+    the indices.
+    """
     for i, id_ in enumerate(ids):
-        # Pretty bad hashing method, but our IDs are already almost
-        # uniform, they just need a little bit of help :)
-        # Hashing method due to Knuth.
-        id_ = nb.u4(id_ * nb.u4(0x9e3779b1))
-        id_ = id_ % arr_size
-        while id2ind[id_] != SENTINEL:
-            id_ += 1
-            if id_ >= arr_size:
-                id_ -= arr_size
-        id2ind[id_] = i + offset
+        # Pretty bad hashing method (by Knuth), but our IDs are already
+        # almost uniform, they just need a little bit of help :)
+        hash_ = nb.u4(id_ * nb.u4(0x9e3779b1)) % arr_size
+        while id2ind[hash_] != SENTINEL:  # Cell not free.
+            hash_ += 1
+            if hash_ >= arr_size:
+                hash_ = 0  # Gone past the end; wraparound
+        id2ind[hash_] = i + offset
 
 
 def get_hash_map_size(i):
+    """Get hash map array length from number of stored elements.
+
+    Compute (number of stored elements * 1.5), rounded up to a power of
+    2. This gives an occupancy rate between 1/3 and 2/3.
+    """
     i += i // 2
     return 1 << i.bit_length()
 
 
 def make_id_hash_map(ids, path, offset=0):
+    """Make ID to index hash map.
+
+    ids is an ordered NumPy array of IDs. path is the destination.
+    offset is added to the indices before storing.
+    """
     with open(path, 'wb+') as f:
+        # Trick: seek past the end of file and write one byte to
+        # efficiently zero-fill up to that point.
         f.seek(get_hash_map_size(len(ids)) * np.uint32().nbytes - 1)
         f.write(b'\x00')
         f.flush()
         id2ind_mmap = mmap.mmap(f.fileno(), 0)
     with id2ind_mmap:
         id2ind = np.frombuffer(id2ind_mmap, dtype=np.uint32)
-        id2ind[...] = SENTINEL
+        id2ind[...] = SENTINEL  # Important detail: set all to SENTINEL.
         arr_size = len(id2ind)
+        # nb.literally causes Numba to compile the function once for
+        # every value. Avoids the CPU's slow division instruction.
         _make_hash_map(ids, id2ind, nb.literally(arr_size), offset)
--- konigsberg/sparseutil.py
+++ konigsberg/sparseutil.py
@@ -1,3 +1,15 @@
+"""Utilities for sparse arrays.
+
+The sparse array format is based on the compressed sparse row format.
+See https://en.wikipedia.org/wiki/Sparse_matrix#Compressed_sparse_row_(CSR,_CRS_or_Yale_format).
+
+The three differenes are:
+1. No value is stored for an array cell.
+2. Duplicate entries are permitted.
+3. One structure can be used to store multiple maps with the same
+   domain by interleaving their data.
+"""
+
 import mmap
 
 import numba as nb
@@ -7,11 +19,31 @@ from numba.types import List, Tuple
 
 @nb.njit(nogil=True)
 def make_counts(maps, ptr_arr):
+    """Count number of edges per index (in the domain) with accumulation.
+    
+    In other words, for every input index, ptr_arr[index] is first set
+    to the number of elements that index maps to. A cumulative sum is
+    then performed over ptr_arr.
+
+    The result tells us the slice of ind_arr that corresponds to every
+    index. The elements that index maps to are
+    ind_arr[ptr_arr[index-1]:ptr_arr[index]] (note that this is
+    slightly different than the final result). This is needed to place
+    elements in ind_arr.
+
+    maps is a list of maps. Each map is itself a list of 2-tuples of
+    arrays (from index, to index); the to index is ignored here. Within
+    each map, the arrays are treated as if they were concatenated. All
+    maps must have the same domain, but they don't necessarily have to
+    have the same codomain.
+    """
     number_maps = len(maps)
+    # Account for multiple maps. ptr_arr_2d[domain_index, map_index].
     ptr_arr_2d = ptr_arr[:-1].reshape(-1, number_maps)
     for i, map_ in enumerate(maps):
         map_ptr = ptr_arr_2d[...,i]
         for from_arr, _ in map_:
+            # As if all arrays in map_ were concatenated.
             for j in from_arr:
                 map_ptr[j] += 1
 
@@ -22,15 +54,25 @@ def make_counts(maps, ptr_arr):
 
 @nb.njit(nogil=True)
 def place_indices(maps, ptr_arr, ind_arr):
+    """Place codomain elements in ind_arr.
+
+    In the process, each element of ptr_arr is decremented by the number
+    of elements that the corresponding index maps to.
+    """
     number_maps = len(maps)
     ptr_arr_2d = ptr_arr[:-1].reshape(-1, number_maps)
     for i, map_ in enumerate(maps):
         map_ptr = ptr_arr_2d[...,i]
         for from_arr, to_arr in map_:
+            # Treat all arrays in map_ as concatenated.
             for j, k in zip(from_arr, to_arr):
+                # It's not obvious that this gives the correct result,
+                # but try it on a piece of paper. map_ptr[j] - 1 is the
+                # last free spot. Decrement map_ptr[j] in-place by 1.
                 index = map_ptr[j] = map_ptr[j] - 1
                 ind_arr[index] = k
 
+    # Sort the image of every index.
     for i in range(len(ptr_arr) - 1):
         start = ptr_arr[i]
         end = ptr_arr[i + 1]
@@ -38,7 +80,23 @@ def place_indices(maps, ptr_arr, ind_arr):
             ind_arr[start:end].sort()
 
 
-def make_sparse_matrix(n_from_ids, maps, ptr_path, ind_path):
+def make_sparse_matrix(n_from_ind, maps, ptr_path, ind_path):
+    """Make sparse matrix representing one of more maps.
+
+    n_from_ind is the number of indices in the domain. maps is a list of
+    maps to include in the matrix. ptr_path and ind_path are the
+    destinations for the pointer array and the indices array,
+    respectively.
+
+    Each map in maps is a list of 2-tuples of NumPy array. This is for
+    convenience: each map is treated as the concatenation of all the
+    arrays it contains. Each tuple contains a series of domain indices
+    and a series of the corresponding codomain indices. They must be of
+    the same length and need not be sorted or deduplicated. Separate
+    maps are stored separately, but interleaved for locality of
+    reference.
+    """
+    # Convert to types Numba will understand.
     maps = nb.typed.List([
         nb.typed.List([
             (from_series.to_numpy(), to_series.to_numpy())
@@ -47,7 +105,9 @@ def make_sparse_matrix(n_from_ids, maps, ptr_path, ind_path):
     n_maps = len(maps)
     n_links = sum(sum(len(s) for s, _ in map_) for map_ in maps)
     with open(ptr_path, 'wb+') as f_ptr:
-        f_ptr.seek((n_maps * n_from_ids + 1) * np.uint32().nbytes - 1)
+        # Trick: seek past the end of file and write one byte to
+        # efficiently zero-fill up to that point.
+        f_ptr.seek((n_maps * n_from_ind + 1) * np.uint32().nbytes - 1)
         f_ptr.write(b'\x00')
         f_ptr.flush()
         map_ptr = mmap.mmap(f_ptr.fileno(), 0)
@@ -55,6 +115,7 @@ def make_sparse_matrix(n_from_ids, maps, ptr_path, ind_path):
         arr_ptr = np.frombuffer(map_ptr, dtype=np.uint32)
         make_counts(maps, arr_ptr)
         with open(ind_path, 'wb+') as f_ind:
+            # Same trick.
             f_ind.seek(n_links * np.uint32().nbytes - 1)
             f_ind.write(b'\x00')
             f_ind.flush()
