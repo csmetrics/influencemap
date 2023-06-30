b7fc4f915e159975f081ec99dc2417840cdeda8e
Minjeong Shin <minjeong.shin@anu.edu.au>
2023-06-30 12:10:11 +0900

    query openalex to get paper info using MAG id

--- core/search/query_info.py
+++ core/search/query_info.py
@@ -1,38 +1,51 @@
-'''
-Functions which combines the different query functions in query_db and
-query_cache.
+import requests
 
-date:   24.06.18
-author: Alexander Soen
-'''
+WORK_FILTER_URL_MAG = "https://api.openalex.org/works"
 
-from core.search.elastic import client
-from elasticsearch_dsl import Search, Q
-from graph.config import conf
+def query_openalex_by_mag_id(mag_id):
+    params = {
+        'filter': 'ids.mag:'+mag_id,
+        'mailto': 'minjeong.shin@anu.edu.au'
+    }
+    try:
+        response = requests.get(WORK_FILTER_URL_MAG, params=params)
+        if response.status_code == 200:
+            data = response.json()
+            return data['results'][0]
+        else:
+            print("[query_openalex_by_mag_id] Request failed with status code:", response.status_code)
+    except requests.exceptions.RequestException as e:
+        print("[query_openalex_by_mag_id] An error occurred:", e)
+    return {}
 
-TIMEOUT = 60
 
 def papers_prop_query(paper_ids):
     ''' Get properties of a paper.
     '''
+
     # Targets
     papers_targets = ['DocType', 'OriginalTitle', 'OriginalVenue', 'Year']
 
+    # raise Exception(len(paper_ids), paper_ids)
     # Query for papers
-    papers_s = Search(index = conf.get('index.paper'), using = client)
-    papers_s = papers_s.query('terms', PaperId=paper_ids)
-    papers_s = papers_s.source(papers_targets)
-    papers_s = papers_s.params(request_timeout=TIMEOUT)
+    papers_s = {mag_id:query_openalex_by_mag_id(mag_id) for mag_id in paper_ids}
 
     # Convert papers into dictionary format
     results = dict()
-    conf_ids = set()
-    jour_ids = set()
-    for paper in papers_s.scan():
-        # Get properties for papers
-        paper_res = paper.to_dict()
-        p_id = int(paper.meta.id)
-        paper_res['PaperId'] = p_id
-        results[p_id] = paper_res
+    for p_id, paper in papers_s.items():
+        try:
+            try:
+                venue = paper['primary_location']['source']['host_organization_name']
+            except Exception:
+                venue = None
+
+            results[p_id] = {
+                'PaperId': p_id,
+                'OriginalTitle': paper['title'],
+                'OriginalVenue': venue,
+                'Year': paper['publication_year']
+            }
+        except Exception:
+            raise Exception(p_id, paper)
 
     return results
