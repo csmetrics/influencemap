1db2180a80a90b1840a3c885da4dbe65346af23c
Minjeong Shin <minjeong.shin@anu.edu.au>
2023-07-19 23:55:54 +0900

    add functions to generate paper relations

--- konigsberg/preprocessor.py
+++ konigsberg/preprocessor.py
@@ -93,11 +93,113 @@ def generate_works_file(data_path):
     logger.info('...done {} {}'.format(data_type, split_char))
 
 
+def generate_paper_references(data_path):
+    data_type = 'works'
+    path = data_path / data_type
+    part_files = path.glob('updated_date=*/part_*')
+    outfile = data_path / ("PaperReferences" + OUT_SUFF)
+
+    logger.info('...generating PaperReferences')
+    # names=['citor_id', 'citee_id']
+    part_files = path.glob('updated_date=*/part_*')
+
+    for file in part_files:
+        for line in open(file, 'r'):
+            try:
+                json_data = json.loads(line)
+                if len(json_data['referenced_works']) == 0:
+                    continue
+                df_id = pd.DataFrame({'citor_id': [json_data['id'].split('W')[1]]})
+                df_referenced = pd.DataFrame({'citee_id': [ref.split('W')[1] for ref in json_data['referenced_works']]})
+                df_part = pd.merge(df_id, df_referenced, how='cross')
+                df_part.to_csv(outfile, mode='a', index=False, header=False)
+            except json.JSONDecodeError:
+                print(f"Error parsing JSON: {line}")
+    logger.info('...done')
+
+
+def generate_paper_authorships(data_path):
+    data_type = 'works'
+    path = data_path / data_type
+    part_files = path.glob('updated_date=*/part_*')
+    outfile = data_path / ("PaperAuthorAffiliations" + OUT_SUFF)
+
+    logger.info('...generating PaperAuthorAffiliations')
+    # names=['paper_id', 'author_id', 'affiliation_id']
+    part_files = path.glob('updated_date=*/part_*')
+
+    for file in part_files:
+        for line in open(file, 'r'):
+            try:
+                json_data = json.loads(line)
+                filtered_data = {
+                    'paper_id': json_data['id'].split('W')[1],
+                }
+                try:
+                    authors = [a['author']['id'].split('A')[1] for a in json_data['authorships']]
+                except:
+                    authors = []
+                if len(authors) == 0:
+                    continue
+
+                institutions = []
+                for a in json_data['authorships']:
+                    inst = None
+                    try:
+                        inst = a['institutions'][0]['id'].split('I')[1]
+                    except:
+                        pass
+                    institutions.append(inst)
+
+                filtered_data['authors'] = authors
+                filtered_data['institutions'] = institutions
+
+                df_id = pd.DataFrame({'paper_id': [filtered_data['paper_id']]})
+                df_referenced = pd.DataFrame({
+                    'author_id': filtered_data['authors'],
+                    'affiliation_id': filtered_data['institutions']
+                })
+                df_part = pd.merge(df_id, df_referenced, how='cross')
+                df_part.to_csv(outfile, mode='a', index=False, header=False)
+            except Exception as e:
+                print(f"Error parsing JSON: {line}")
+    logger.info('...done')
+
+
+def generate_paper_fos(data_path):
+    data_type = 'works'
+    path = data_path / data_type
+    part_files = path.glob('updated_date=*/part_*')
+    outfile = data_path / ("PaperFieldsOfStudy" + OUT_SUFF)
+
+    logger.info('...generating PaperFieldsOfStudy')
+    # names=['paper_id', 'fos_id']
+    part_files = path.glob('updated_date=*/part_*')
+
+    for file in part_files:
+        for line in open(file, 'r'):
+            try:
+                json_data = json.loads(line)
+                concepts = []
+                for c in json_data['concepts']:
+                    if c['level'] <= 1:
+                        concepts.append(c['id'].split('C')[1])
+                if len(concepts) == 0:
+                    continue
+                df_id = pd.DataFrame({'paper_id': [json_data['id'].split('W')[1]]})
+                df_referenced = pd.DataFrame({'fos_id': concepts})
+                df_part = pd.merge(df_id, df_referenced, how='cross')
+                df_part.to_csv(outfile, mode='a', index=False, header=False)
+            except json.JSONDecodeError:
+                print(f"Error parsing JSON: {line}")
+    logger.info('...done')
+
+
 def generate_text_files(data_path):
     """Run the script, reading from in_path and writing to out_path."""
     data_path = pathlib.Path(data_path)
 
-    logger.info('(0/2) read and generate entity files')
+    logger.info('(0/5) read and generate entity files')
     for path_type, split_char in [
         ['authors', 'A'],
         ['institutions', 'I'],
@@ -106,10 +208,19 @@ def generate_text_files(data_path):
     ]:
         generate_entity_files(data_path, path_type, split_char)
 
-    logger.info('(1/2) read and generate work files')
+    logger.info('(1/5) read and generate work files')
     generate_works_file(data_path)
 
-    logger.info('(2/2) done')
+    logger.info('(2/5) create PaperReferences')
+    generate_paper_references(data_path)
+
+    logger.info('(3/5) create PaperAuthorAffiliations')
+    generate_paper_authorships(data_path)
+
+    logger.info('(4/5) create PaperFieldsOfStudy')
+    generate_paper_fos(data_path)
+
+    logger.info('(5/5) done')
 
 
 def main():
